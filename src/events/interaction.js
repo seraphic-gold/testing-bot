dcc301bb59f9a7d43e09283450dcbfdbd3cb88e5
seraphic-gold <seraphic-gold@users.noreply.github.com>
2025-09-10 00:27:49 +1000

    Turned into Template

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -3,9 +3,7 @@ import { Events, MessageFlags } from "discord.js";
 const name = Events.InteractionCreate;
 const execute = async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  console.log(interaction.client.commands);
   const command = interaction.client.commands.get(interaction.commandName);
-  console.log(command);
 
   if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
